b60e0dbfe68112511a5fdaf77815a2734477d753
Kim Robinson <kimmykokonut@gmail.com>
2024-01-12 18:46:19 -0800

    add constructor

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -3,8 +3,15 @@ namespace ShapeTracker.Models
   public class Triangle
   {
     public int Side1;
-    public int Side2 = 4;
+    public int Side2;
     public int Side3;
+
+    public Triangle(int length1, int length2, int length3)
+    {
+      Side1 = length1;
+      Side2 = length2;
+      Side3 = length3;
+    }
   }
 
 }
--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -8,10 +8,8 @@ namespace ShapeTracker
   {
     static void Main()
     {
-      Triangle testTriangle = new Triangle();
+      Triangle testTriangle = new Triangle(3,4,5);
       Console.WriteLine(testTriangle.GetType());
-      testTriangle.Side1 = 65;
-      testTriangle.Side3 = 88;
       Console.WriteLine($"Side one of triangle: {testTriangle.Side1} ");
       Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
       Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
