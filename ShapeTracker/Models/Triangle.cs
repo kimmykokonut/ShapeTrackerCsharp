163c0537a6d77e05251f0c92dacd319dffc7d7ae
Kim Robinson <kimmykokonut@gmail.com>
2024-01-12 18:33:04 -0800

    add classes and namespaces

---
+++ ShapeTracker/Models/Triangle.cs
@@ -0,0 +1,10 @@
+namespace ShapeTracker.Models
+{
+  public class Triangle
+  {
+    public int Side1;
+    public int Side2 = 4;
+    public int Side3;
+  }
+
+}
---
+++ ShapeTracker/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using ShapeTracker.Models;
+
+namespace ShapeTracker
+{
+
+  class Program
+  {
+    static void Main()
+    {
+      Triangle testTriangle = new Triangle();
+      Console.WriteLine(testTriangle.GetType());
+    }
+  }
+}
