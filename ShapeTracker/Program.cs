643826b3dbcd4bce2a51a5e9130d27e1fb9259a3
Kim Robinson <kimmykokonut@gmail.com>
2024-01-12 18:39:11 -0800

    add class fields

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -4,12 +4,17 @@ using ShapeTracker.Models;
 namespace ShapeTracker
 {
 
-  class Program
+  public class Program
   {
     static void Main()
     {
       Triangle testTriangle = new Triangle();
       Console.WriteLine(testTriangle.GetType());
+      testTriangle.Side1 = 65;
+      testTriangle.Side3 = 88;
+      Console.WriteLine($"Side one of triangle: {testTriangle.Side1} ");
+      Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
+      Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
     }
   }
 }
