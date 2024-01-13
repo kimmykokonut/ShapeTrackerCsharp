916ffb83d8d929a224db40b398c5326dc5c119c2
Kim Robinson <kimmykokonut@gmail.com>
2024-01-12 21:07:20 -0800

    add getter method

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -10,9 +10,9 @@ namespace ShapeTracker
     {
       Triangle testTriangle = new Triangle(17,4,4);
       Console.WriteLine(testTriangle.GetType());
-      Console.WriteLine($"Side one of triangle: {testTriangle.Side1} ");
-      Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
-      Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
+      Console.WriteLine($"Side one of triangle: {testTriangle.GetSide1()} ");
+      Console.WriteLine($"Side two of the triangle: {testTriangle.GetSide2()}");
+      Console.WriteLine($"Side three of the triangle: {testTriangle.GetSide3()}");
       Console.WriteLine("Want to know what type of triangle?");
       Console.WriteLine("calculating...");
       Console.WriteLine($"Your triangle is: {testTriangle.CheckType()}");
