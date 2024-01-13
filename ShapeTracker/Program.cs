103ea835a8a07dc449f7d9a26b3280b58f5e2637
Kim Robinson <kimmykokonut@gmail.com>
2024-01-12 18:58:22 -0800

    add class methods to calculate triangle type

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -8,11 +8,14 @@ namespace ShapeTracker
   {
     static void Main()
     {
-      Triangle testTriangle = new Triangle(3,4,5);
+      Triangle testTriangle = new Triangle(17,4,4);
       Console.WriteLine(testTriangle.GetType());
       Console.WriteLine($"Side one of triangle: {testTriangle.Side1} ");
       Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
       Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
+      Console.WriteLine("Want to know what type of triangle?");
+      Console.WriteLine("calculating...");
+      Console.WriteLine($"Your triangle is: {testTriangle.CheckType()}");
     }
   }
 }
