126b82b0e505aa27c88b11dfece0cfd59191f7c1
Kim Robinson <kimmykokonut@gmail.com>
2024-01-16 14:54:00 -0800

    pass test and add get side2 rectangle
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -10,7 +10,7 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
     {
-      Rectangle newRectangle = new Rectangle(2);
+      Rectangle newRectangle = new Rectangle(2, 5);
       Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
     }
 
@@ -18,7 +18,7 @@ namespace ShapeTracker.Tests
     public void GetSide1_ReturnsSide1_Int()
     {
       int length1 = 3;
-      Rectangle newRectangle = new Rectangle(length1);
+      Rectangle newRectangle = new Rectangle(length1, 5);
       int result = newRectangle.Side1;
       Assert.AreEqual(length1, result);
     }
@@ -26,10 +26,19 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void SetSide1_SetsValueOfSide1_Void()
     {
-      Rectangle newRectangle = new Rectangle(3);
+      Rectangle newRectangle = new Rectangle(3, 5);
       int newLength1 = 44;
       newRectangle.Side1 = newLength1;
       Assert.AreEqual(newLength1, newRectangle.Side1);
+    }
+
+    [TestMethod]
+    public void GetSide2_ReturnsSide2_Int()
+    {
+      int length2 = 3;
+      Rectangle newRectangle = new Rectangle (2, length2);
+      int result = newRectangle.Side2;
+      Assert.AreEqual(length2, result);
     }
 
   }
--- ShapeTracker/Models/Rectangle.cs
+++ ShapeTracker/Models/Rectangle.cs
@@ -8,9 +8,11 @@ namespace ShapeTracker.Models
       get { return _side1; }
       set { _side1 = value; }
     }
-    public Rectangle(int length1)
+    public int Side2 { get; }
+    public Rectangle(int length1, int length2)
     {
       _side1 = length1;
+      Side2 = length2;
     }
   }
 }
