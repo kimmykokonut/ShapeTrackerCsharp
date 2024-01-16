0771b9d3c2467399e29f891cc072d0737650db66
Kim Robinson <kimmykokonut@gmail.com>
2024-01-15 20:46:46 -0800

    initial setup for MSTest

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -4,57 +4,57 @@ namespace ShapeTracker.Models
 {
   public class Triangle
   {
-    private int _side1;
-    public int Side1
-    {
-      get { return _side1; }
-      set { _side1 = value; }
-    }
-    public int Side2 { get; set; }  //creates a private _side2 field. not visible b/c implicit.
-    private int _side3;
-    private static List<Triangle> _instances = new List<Triangle> {};
+    // private int _side1;
+    // public int Side1
+    // {
+    //   get { return _side1; }
+    //   set { _side1 = value; }
+    // }
+    // public int Side2 { get; set; }  //creates a private _side2 field. not visible b/c implicit.
+    // private int _side3;
+    // private static List<Triangle> _instances = new List<Triangle> {};
 
-    public Triangle(int length1, int length2, int length3)
-    {
-      _side1 = length1;
-      Side2 = length2;//for auto-impl
-      _side3 = length3;
-      _instances.Add(this);
-    }
-    public int GetSide3()
-    {
-      return _side3;
-    }
-    public void SetSide3(int newSide)
-    {
-      _side3 = newSide;
-    }
-    public string CheckType()
-    {
-      if ((_side1 > (Side2 + _side3)) || (Side2 > (_side1 + _side3)) || (_side3 > (_side1 + Side2)))
-      {
-      return "not a triangle";
-      }
-      else if ((_side1 != Side2) && ((_side1 != _side3)) && ((Side2 != _side3)))
-      {
-        return "scalene triangle";
-      }
-      else if ((_side1 == Side2) && (_side1 == _side3))
-      {
-        return "equilateral triangle";
-      }
-      else 
-      {
-        return "isosceles triangle";
-      }
-    }
-    public static List<Triangle> GetAll()
-    {
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
+    // public Triangle(int length1, int length2, int length3)
+    // {
+    //   _side1 = length1;
+    //   Side2 = length2;//for auto-impl
+    //   _side3 = length3;
+    //   _instances.Add(this);
+    // }
+    // public int GetSide3()
+    // {
+    //   return _side3;
+    // }
+    // public void SetSide3(int newSide)
+    // {
+    //   _side3 = newSide;
+    // }
+    // public string CheckType()
+    // {
+    //   if ((_side1 > (Side2 + _side3)) || (Side2 > (_side1 + _side3)) || (_side3 > (_side1 + Side2)))
+    //   {
+    //   return "not a triangle";
+    //   }
+    //   else if ((_side1 != Side2) && ((_side1 != _side3)) && ((Side2 != _side3)))
+    //   {
+    //     return "scalene triangle";
+    //   }
+    //   else if ((_side1 == Side2) && (_side1 == _side3))
+    //   {
+    //     return "equilateral triangle";
+    //   }
+    //   else 
+    //   {
+    //     return "isosceles triangle";
+    //   }
+    // }
+    // public static List<Triangle> GetAll()
+    // {
+    //   return _instances;
+    // }
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
   }
 }
---
+++ ShapeTracker/UserInterfaceModels/Banners.cs
@@ -0,0 +1,24 @@
+namespace ShapeTracker.UserInterfaceModels
+{
+  class WelcomeBanner
+  {
+    public static string Welcome { get; } = @"
+  #####                                #######                                           
+ #     # #    #   ##   #####  ######      #    #####    ##    ####  #    # ###### #####  
+ #       #    #  #  #  #    # #           #    #    #  #  #  #    # #   #  #      #    # 
+  #####  ###### #    # #    # #####       #    #    # #    # #      ####   #####  #    # 
+       # #    # ###### #####  #           #    #####  ###### #      #  #   #      #####  
+ #     # #    # #    # #      #           #    #   #  #    # #    # #   #  #      #   #  
+  #####  #    # #    # #      ######      #    #    # #    #  ####  #    # ###### #    # 
+                                                                                         ";
+
+  }
+  class GoodbyeBanner
+  {
+    //code
+  }
+  class SubMenuBanner
+  {
+    //code
+  }
+}
