85486d0d79167b835db2d21ba422f12b9ed546ef
Kim Robinson <kimmykokonut@gmail.com>
2024-01-16 14:42:54 -0800

    pass test and add side1 get functionality
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -10,9 +10,19 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
     {
-      Rectangle newRectangle = new Rectangle();
+      Rectangle newRectangle = new Rectangle(2);
       Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
     }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      int length1 = 3;
+      Rectangle newRectangle = new Rectangle(length1);
+      int result = newRectangle.Side1;
+      Assert.AreEqual(length1, result);
+    }
+
 
   }
 }
--- ShapeTracker/Models/Rectangle.cs
+++ ShapeTracker/Models/Rectangle.cs
@@ -2,9 +2,14 @@ namespace ShapeTracker.Models
 {
   public class Rectangle
   {
-    public Rectangle()
+    private int _side1;
+    public int Side1
     {
-      
+      get { return _side1; }
+    }
+    public Rectangle(int length1)
+    {
+      _side1 = length1;
     }
   }
 }
