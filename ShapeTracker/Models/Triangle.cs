68ebe7f8abdee430a9e136a2f31ecbcbb8d94d32
Kim Robinson <kimmykokonut@gmail.com>
2024-01-16 09:54:03 -0800

    add passing test, code for Triangle.Side1 SET

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,13 +1,19 @@
-using System.Collections.Generic; //directive
 
 namespace ShapeTracker.Models
 {
   public class Triangle
   {
-    // private int _side1;
-    // public int Side1
-    // {
-    //   get { return _side1; }
+    private int _side1;
+    public int Side1
+    {
+      get { return _side1; }
+      set { _side1 = value; }
+    }
+    public Triangle(int length1)
+    {
+      _side1 = length1;
+    }
+    
     //   set { _side1 = value; }
     // }
     // public int Side2 { get; set; }  //creates a private _side2 field. not visible b/c implicit.
