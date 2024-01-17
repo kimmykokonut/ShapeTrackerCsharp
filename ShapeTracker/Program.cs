cd2cdd7f7f8e2ac1c277618d4c06c2ee8c193519
Kim Robinson <kimmykokonut@gmail.com>
2024-01-16 16:07:06 -0800

    add make rectangle and calc area to ui
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -11,9 +11,18 @@ namespace ShapeTracker
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker app!");
-      Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
-      CreateTriangle();
-
+      Console.WriteLine("Choose 't' to make a triangle and determine it's type or 'r' to make a rectangle and calculate it's area...");
+      string userInput = Console.ReadLine(); 
+      if (userInput == "t" || userInput == "T")
+      {
+        Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+        CreateTriangle();
+      }
+      else if (userInput == "r" || userInput == "R")
+      {
+        Console.WriteLine("We'll calculate the area of a rectangle based off of the lengths of the rectangle's 2 sides.");
+        CreateRectangle();
+      }
     }
     static void CreateTriangle()
     {
@@ -116,5 +125,43 @@ namespace ShapeTracker
         Console.WriteLine("Goodbye!");
       }
     }
+    static void CreateRectangle()
+    {
+      Console.WriteLine("Please enter a number:");
+      string stringNumber1 = Console.ReadLine();
+      Console.WriteLine("Enter another number:");
+      string stringNumber2 = Console.ReadLine();
+      int length1 = int.Parse(stringNumber1);
+      int length2 = int.Parse(stringNumber2);
+      Rectangle rect = new Rectangle(length1, length2);
+      ConfirmOrEditRectangle(rect);
+    }
+    static void ConfirmOrEditRectangle(Rectangle rect)
+    {
+      Console.WriteLine("Please confirm that you entered in  your rectangle correctly:");
+      Console.WriteLine($"Side 1 has a length of {rect.Side1}."); //using Properties
+      Console.WriteLine($"Side 2 has a length of {rect.Side2}."); //using auto-impl. properties
+      Console.WriteLine("Is that correct? Enter 'yes' to proceed or 'no' to re-enter the rectangle's sides");
+      string userInput = Console.ReadLine();
+      if (userInput == "yes")
+      {
+        GetRectArea(rect);
+      }
+      else
+      {
+        Console.WriteLine("Let's fix your rectangle. Please enter the 2 sides again: ");
+        CreateRectangle();
+      }
+    }
+    static void GetRectArea(Rectangle rect)
+    {
+      int result = rect.GetArea();
+      Console.WriteLine("-----------------------------------------");
+      Console.WriteLine("Your rectangle's area is: " + result + ".");
+      Console.WriteLine("-----------------------------------------");
+      Main();
+      // instead of main, navigate to a rect-nav or update the nav for triangle/rect.
+    }
+
   }
 }
